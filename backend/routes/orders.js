93cbb5d0bbce36f2224a65ac18cf92ca4cfa6c15
Impactiron <rothmich84@gmail.com>
2025-11-04 15:26:59 +0100

    Add files via upload

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -2,19 +2,10 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 
-// POST - Create new order
 router.post('/', orderController.createOrder);
-
-// GET - Get all orders
 router.get('/', orderController.getAllOrders);
-
-// GET - Get order by ID
 router.get('/:id', orderController.getOrderById);
-
-// GET - Get orders by location
 router.get('/location/:locationId', orderController.getOrdersByLocation);
-
-// GET - Get orders by date range
 router.get('/date-range/:startDate/:endDate', orderController.getOrdersByDateRange);
 
 module.exports = router;
