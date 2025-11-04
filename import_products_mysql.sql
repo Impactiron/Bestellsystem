0a22c803f35585d68146bf3c4205310222ea8c58
Impactiron <rothmich84@gmail.com>
2025-11-04 11:26:40 +0100

    Add product import data and SQL import scripts

---
+++ import_products_mysql.sql
@@ -0,0 +1,8 @@
+-- SQL Import Script for Products
+
+INSERT INTO products (product_name, clustered_name, category_name, subcategory_name, description, packaging, image_url, unit_price, is_active) VALUES
+('GLASFEE ECO', 'Oberflächen-Schnellreiniger, Glasreiniger', 'Chemie', 'Unterhaltsreiniger', 'GLASFEE ECO ist geeignet zur schnellen, streifenfreien Reinigung von Glas, Spiegeln sowie allen glänzenden, wasserfesten Oberflächen.', '500 ml Flasche', '/images/glasfee-eco.jpg', null, true),
+('LEVANTE Eco', 'Alkohol-Glanzreiniger', 'Chemie', 'Unterhaltsreiniger', 'Ökologischer Alkohol-Glanzreiniger.', '10 L Kanister', '/images/levante-eco.jpg', null, true),
+('Duftspray Citrus', 'Raumspray', 'Chemie', 'Unterhaltsreiniger', 'Langanhaltende Raumfrische in Sekundenschnelle.', '500 ml Flasche', '/images/duftspray-citrus.jpg', null, true),
+('MILIZID Eco', 'Sanitärreiniger, Kalklöser', 'Chemie', 'Sanitärreiniger', 'Ökologischer Sanitärreiniger & Kalklöser.', '10 L Kanister', '/images/milizid-eco.jpg', null, true),
+('NOVO Pen-Off', 'Fleckentferner', 'Chemie', 'sonstige Reiniger', 'Gebrauchsfertiger Oberflächenreiniger.', '500 ml Flasche', '/images/novo-pen-off.jpg', null, true);
